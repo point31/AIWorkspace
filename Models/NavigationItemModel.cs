145feec337d90088dcba539fbd26068183443791
point31 <brunch-cane-portal@duck.com>
2026-06-27 00:51:18 +0300

    uig

---
+++ Models/NavigationItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AIWorkspace.Models;
+
+public class NavigationItemModel
+{
+    public string Title { get; set; } = "";
+
+    // Segoe Fluent icon glyph
+    public string Icon { get; set; } = "";
+
+    public bool IsSelected { get; set; }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using AIWorkspace.Models;
+using System.Collections.ObjectModel;
+
+namespace AIWorkspace.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    public ObservableCollection<NavigationItemModel> NavigationItems { get; } =
+[
+    new()
+    {
+        Title="Home",
+        Icon="\uE80F"
+    },
+
+    new()
+    {
+        Title="Chats",
+        Icon="\uE8BD"
+    },
+
+    new()
+    {
+        Title="Prompt Library",
+        Icon="\uE7C3"
+    },
+
+    new()
+    {
+        Title="AI Providers",
+        Icon="\uE9CA"
+    },
+
+    new()
+    {
+        Title="Files",
+        Icon="\uE8B7"
+    },
+
+    new()
+    {
+        Title="Settings",
+        Icon="\uE713"
+    }
+    ];
+}
